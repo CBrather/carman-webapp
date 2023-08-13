de6b9075eaba46687231e400b8bfa211d506bfc4
Christian Brennecke-Räther <c.b.raether@gmail.com>
2023-08-13 21:23:39 +0200

    fix broken antd layout and change handlers
    CU-861n6z6nb

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Space } from 'antd'
 import { ChartConfigForm } from './components/Forms/ChartConfigForm';
 import RadarChartLayout from './components/Charts/RadarChartLayout';
 
 function App() {
 	return (
-		<Container fluid>
-			<Row>
-				<Col md="6">
-					<RadarChartLayout />
-				</Col>
-				<Col md="6">
-					<ChartConfigForm />
-				</Col>
-			</Row>
-		</Container>
+    <Space>
+      <RadarChartLayout />
+      <ChartConfigForm />
+    </Space>
 	);
 }
 
