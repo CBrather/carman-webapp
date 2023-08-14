0e403f08272b805691a5c141d60e8b27fdeb42bf
Christian Brennecke-Räther <christian.brennecke-raether@sematell.com>
2023-08-14 21:55:00 +0200

    add axis selection to config form
    CU-861n76v4y

--- src/components/Forms/AxisConfigForm.tsx
+++ src/components/Forms/AxisConfigForm.tsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from 'react';
 import { TickInput } from './Inputs/TickInput';
 import { Axis } from '../../store/types/RadarChartTypes';
-import { useDispatch } from 'react-redux';
-import { updateAxis } from '../../store/slices/RadarChartConfig';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectSelectedAxis, updateAxis } from '../../store/slices/RadarChartConfig';
 import { Input, Space } from 'antd'
 
-export function AxisConfigForm(props: { index: number; axis: Axis }) {
+export function AxisConfigForm() {
 	const dispatch = useDispatch();
+  const selectedAxis = useSelector(selectSelectedAxis)
+
 	const [tickInputs, setTickInputs] = useState<React.JSX.Element[]>([]);
-	const [label, setLabel] = useState(props.axis.label);
+	const [label, setLabel] = useState(selectedAxis.axis.label);
 
 	const onInputChange = (index: number, label: string): void => {
-		const axis: Axis = JSON.parse(JSON.stringify(props.axis));
+		const axis: Axis = JSON.parse(JSON.stringify(selectedAxis.axis));
 		axis.ticks[index].label = label;
 
-		dispatch(updateAxis({ index: props.index, axis }));
+		dispatch(updateAxis({ index: selectedAxis.index, axis }));
 	};
 
 	useEffect(() => {
 		const newTickInputs: React.JSX.Element[] = [];
-		const { ticks } = props.axis;
+		const { ticks } = selectedAxis.axis;
 
 		for (let i = 0; i < ticks.length; i++) {
 			newTickInputs.push(
@@ -28,21 +30,21 @@ export function AxisConfigForm(props: { index: number; axis: Axis }) {
 		}
 
 		setTickInputs(newTickInputs);
-	}, [props.axis]);
+	}, [selectedAxis]);
 
 	return (
     <>
 			<Space direction="horizontal">
         <Input
-          value={label}
+          value={selectedAxis.axis.label}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
             setLabel(event.target.value);
           }}
           onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
             if (event.key === 'Enter' || event.key === 'Tab') {
-              const axis: Axis = JSON.parse(JSON.stringify(props.axis));
+              const axis: Axis = JSON.parse(JSON.stringify(selectedAxis.axis));
               axis.label = label;
-              dispatch(updateAxis({ index: props.index, axis }));
+              dispatch(updateAxis({ index: selectedAxis.index, axis }));
             }
           }}
         />
