b247557aca65cc310588c52c859ede567ae6e0ab
Christian Brennecke-Räther <c.b.raether@gmail.com>
2023-08-16 20:59:45 +0200

    change primary color

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ConfigProvider, theme } from 'antd'
+import { volcano } from '@ant-design/colors'
 
 import { Provider } from 'react-redux';
 import store from './store/store.ts';
@@ -10,7 +11,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<Provider store={store}>
-    <ConfigProvider theme={{algorithm: theme.darkAlgorithm}}>
+    <ConfigProvider theme={{algorithm: theme.darkAlgorithm, token: { colorPrimary: volcano.primary}}}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
