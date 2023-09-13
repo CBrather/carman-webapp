b7c169ede64f5e51763719b278271bdd05ef0d99
Christian Brennecke-Räther <CBrather@users.noreply.github.com>
2023-09-13 14:36:11 +0200

    Create vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		coverage: {
+			all: true,
+			enabled: true,
+			include: ['src/**/*.{js,jsx,ts,tsx}'],
+			provider: 'v8',
+			reportsDirectory: 'test/coverage'
+		},
+		outputFile: 'test/results.xml',
+		reporters: ['junit']
+	}
+});
