c2ecfdde3c9d2beba00b18aa6bee969cfc37075e
Christian Brennecke-Räther <CBrather@users.noreply.github.com>
2023-09-21 17:02:10 +0200

    fix linter errors, add testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,58 @@
-import { Layout, Menu } from 'antd'
-import {
-  EditOutlined,
-  RadarChartOutlined,
-} from '@ant-design/icons'
-import { useLocation, useNavigate, Route, Routes } from 'react-router-dom'
+import { Layout, Menu } from 'antd';
+import { EditOutlined, RadarChartOutlined } from '@ant-design/icons';
+import { useLocation, useNavigate, Route, Routes } from 'react-router-dom';
 import ChartLayoutDesigner from './pages/ChartLayoutDesigner';
 import ModelEditor from './pages/ModelEditor';
 
 const { Content, Sider } = Layout;
 
 function App() {
-  let navigate = useNavigate();
-  const selectedKey = useLocation().pathname
+	const navigate = useNavigate();
+	const selectedKey = useLocation().pathname;
 
-  const highlight = () => {
-    if (selectedKey === '/editor'){
-      return ['editor']
-    } else if (selectedKey === '/designer'){
-      return ['designer']
-    }  
-  }
+	const highlight = () => {
+		if (selectedKey === '/editor') {
+			return ['editor'];
+		} else if (selectedKey === '/designer') {
+			return ['designer'];
+		}
+	};
 	return (
-    <Layout>
-      <Sider trigger={null}>
-        <Menu
-          mode="inline"
-          theme="dark"
-          defaultSelectedKeys={['editor']}
-          selectedKeys={highlight()}
-          style={{ height: '100%', borderRight:0 }}
-          items={[
-            {
-              key: 'editor',
-              icon: <RadarChartOutlined />,
-              label: "Editor",
-              onClick: () => { navigate('/editor')}
-            },
-            {
-              key: 'designer',
-              icon: <EditOutlined />,
-              label: "Designer",
-              onClick: () => { navigate('/designer')}
-            }
-
-          ]}
-        />
-      </Sider>
-      <Content>
-        <Routes>
-          <Route path="/editor" element={<ModelEditor />} />
-          <Route path="/designer" element={<ChartLayoutDesigner />} />
-        </Routes>
-      </Content>
-  </Layout>
+		<Layout>
+			<Sider trigger={null}>
+				<Menu
+					mode="inline"
+					theme="dark"
+					defaultSelectedKeys={['editor']}
+					selectedKeys={highlight()}
+					style={{ height: '100%', borderRight: 0 }}
+					items={[
+						{
+							key: 'editor',
+							icon: <RadarChartOutlined />,
+							label: 'Editor',
+							onClick: () => {
+								navigate('/editor');
+							}
+						},
+						{
+							key: 'designer',
+							icon: <EditOutlined />,
+							label: 'Designer',
+							onClick: () => {
+								navigate('/designer');
+							}
+						}
+					]}
+				/>
+			</Sider>
+			<Content>
+				<Routes>
+					<Route path="/editor" element={<ModelEditor />} />
+					<Route path="/designer" element={<ChartLayoutDesigner />} />
+				</Routes>
+			</Content>
+		</Layout>
 	);
 }
 
