da914b74a9b8cd8ba89126610f7ae76b1bc505af
Christian Brennecke-Räther <CBrather@users.noreply.github.com>
2023-09-21 15:17:51 +0000

    fix linter errors

--- src/store/slices/RadarChartConfig.test.ts
+++ src/store/slices/RadarChartConfig.test.ts
@@ -2,14 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { expect, it, describe } from 'vitest';
 import { chartConfig } from './RadarChartConfig';
 
-
 describe('Slice: RadarChartConfig', () => {
 	it('initializes with empty user non-authorized user', () => {
-        const store = configureStore({
-            reducer: chartConfig.reducer
-        });
+		const store = configureStore({
+			reducer: chartConfig.reducer
+		});
 
-        const defaultState = store.getState();
+		const defaultState = store.getState();
 		expect(defaultState.selectedAxis).toBe(0);
 	});
 });
