eca70339674282238efc666407dfbaad2f3e20a4
CBrather <c.b.raether@gmail.com>
2024-01-22 21:10:59 +0100

    make component props readonly and move svgedge

---
+++ src/components/Charts/SVGEdge.tsx
@@ -0,0 +1,31 @@
+import { MouseEventHandler } from 'react';
+import { buildSVGPath } from '../../services/SVGPathBuilder';
+import { EdgeDesign, EdgeStyle } from '../../store/slices/RadarChartDesign';
+import { Coordinate2D } from '../../store/types/RadarChartTypes';
+
+type Props = {
+	readonly design: EdgeDesign;
+	readonly points: Coordinate2D[];
+	readonly className?: string;
+	readonly closed?: boolean;
+	readonly onClick?: MouseEventHandler<SVGPathElement>;
+};
+
+export default function SVGEdge(props: Props) {
+	const { design, points } = props;
+
+	if (!points || points.length === 0) return;
+
+	const path = buildSVGPath(points, !!props.closed);
+
+	return (
+		<path
+			className={props.className ?? ''}
+			strokeDasharray={design.style == EdgeStyle.Dashed ? 5 : 0}
+			stroke={design.color}
+			strokeWidth={design.thickness}
+			d={path}
+			onClick={props.onClick}
+		/>
+	);
+}
