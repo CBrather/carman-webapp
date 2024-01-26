47a4155edaf0c8356f76d1f8ddd3001165aedd54
CBrather <c.b.raether@gmail.com>
2024-01-26 20:25:33 +0100

    fix rtk query headers hook not finding auth client

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,25 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { chartDesign, SLICE_NAME as CHART_DESIGN_SLICE_NAME } from './slices/RadarChartDesign';
 import { dataSet, SLICE_NAME as DATA_SET_SLICE_NAME } from './slices/DataSet';
+import { api } from '../api/api.gen';
+import { Auth0Client } from '@auth0/auth0-spa-js';
+import { GetEnvironmentConfig } from '../services/Environment';
+
+const { audience, clientID, domain } = GetEnvironmentConfig().auth;
 
 const store = configureStore({
 	reducer: combineReducers({
 		[CHART_DESIGN_SLICE_NAME]: chartDesign.reducer,
 		[DATA_SET_SLICE_NAME]: dataSet.reducer
-	})
+	}),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			thunk: {
+				extraArgument: {
+					authClient: new Auth0Client({ domain, clientId: clientID, authorizationParams: { audience } })
+				}
+			}
+		}).concat(api.middleware)
 });
 
 export default store;
