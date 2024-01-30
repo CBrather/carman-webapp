17a73ec025d2f22f664c769c4640cd233f9d20f8
CBrather <c.b.raether@gmail.com>
2024-01-30 14:58:36 +0100

    fix code smells reported by sonar

--- src/store/wrappers.ts
+++ src/store/wrappers.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AnyAction, CombinedState, Dispatch, EmptyObject, ThunkDispatch, createAsyncThunk } from '@reduxjs/toolkit';
+import { AnyAction, ThunkDispatch, createAsyncThunk } from '@reduxjs/toolkit';
 import { Auth0Client } from '@auth0/auth0-spa-js';
 import { ChartDesignState, SLICE_NAME as CHART_DESIGN_SLICE_NAME } from './slices/RadarChartDesign';
 import { DataSetState, SLICE_NAME as DATA_SET_SLICE_NAME } from './slices/DataSet';
@@ -16,9 +16,9 @@ export type ThunkExtra = {
 
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{ extra: ThunkExtra; rejectValue: string }>();
 
-export type RootState = EmptyObject & {
+export type RootState = {
 	[DATA_SET_SLICE_NAME]: DataSetState;
 	[CHART_DESIGN_SLICE_NAME]: ChartDesignState;
 };
 
-export type AppDispatch = ThunkDispatch<CombinedState<RootState>, ThunkExtra, AnyAction> & Dispatch<AnyAction>;
+export type AppDispatch = ThunkDispatch<RootState, ThunkExtra, AnyAction>;
